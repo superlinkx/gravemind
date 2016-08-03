a257a875088ed03cdde74ca32d91d20ef8cb97a1
Steven Holms <superlinkx@gmail.com>
2016-08-02 20:06:21 -0600

    Updated comma implementation to actually fix the fixed-decimal issue
    Fixes #13 by adding implementation of payments summary table

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/shopspring/decimal"
@@ -49,8 +51,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func commaString(dec decimal.Decimal, places int32) string {
-	floatConv, _ := strconv.ParseFloat(dec.StringFixed(places), 64)
-	return humanize.Commaf(floatConv)
+	var decbuffer bytes.Buffer
+	var fullnum bytes.Buffer
+	decFloat, _ := strconv.ParseFloat(dec.StringFixed(places), 64)
+
+	decString := humanize.Commaf(decFloat)
+	parts := strings.Split(decString, ".")
+
+	if len(parts) == 2 {
+		decbuffer.WriteString(parts[1])
+	} else if len(parts) == 1 {
+		decbuffer.WriteString("")
+	} else {
+		return ""
+	}
+
+	for decbuffer.Len() < int(places) {
+		decbuffer.WriteString("0")
+	}
+
+	fullnum.WriteString(parts[0])
+	if places > 0 {
+		fullnum.WriteString(".")
+		fullnum.WriteString(decbuffer.String())
+	}
+
+	return fullnum.String()
 }
 
 func error500(w *http.ResponseWriter, msg string, err error) {
